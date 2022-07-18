e7118e40d0cfb9f7dec70eb3fcd1f972d66bf47f
Shamimzz <shamimislam5091@gmail.com>
2022-07-18 12:30:34 +0600

    counter

--- src/feathers/counter/CounterView.jsx
+++ src/feathers/counter/CounterView.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const CounterView = () => {
-  return <div>CounterView</div>;
+  const count = useSelector((state) => state.counter.count);
+
+  return (
+    <div>
+      <h1>CounterView</h1>
+
+      <h3>Count : {count}</h3>
+      <button>Increment</button>
+      <button>Decrement</button>
+      <button>Reset</button>
+    </div>
+  );
 };
 
 export default CounterView;
--- src/feathers/counter/counterSlice.jsx
+++ src/feathers/counter/counterSlice.jsx
@@ -1,4 +1,3 @@
-// const { createSlice } = require("@reduxjs/toolkit");
 import { createSlice } from "@reduxjs/toolkit";
 
 export const counterSlice = createSlice({
